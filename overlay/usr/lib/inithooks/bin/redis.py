3709c5c6e1005379b63df6924c552ae7c10aa4af
Jeremy Davis <jeremy@turnkeylinux.org>
2020-06-26 13:46:20 +1000

    Make redis inithooks PEP8 compliant.

--- overlay/usr/lib/inithooks/bin/redis.py
+++ overlay/usr/lib/inithooks/bin/redis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Set Redis-commander password and Redis remote IP bind and protected-mode directives
+"""Set Redis-commander password and Redis remote IP bind and protected-mode
+directives.
 
 Option:
     --ip_bind=         unless provided, will ask interactively
@@ -10,9 +11,11 @@ Option:
 import sys
 import getopt
 import hashlib
-
 import subprocess
+
 from dialog_wrapper import Dialog
+
+DEFAULT_BIND = "0.0.0.0"
 
 
 def usage(s=None):
@@ -22,7 +25,6 @@ def usage(s=None):
     print(__doc__, file=sys.stderr)
     sys.exit(1)
 
-DEFAULT_BIND = "0.0.0.0"
 
 def validate_ip(ipaddr):
     if (ipaddr.count('.') != 3):
@@ -33,10 +35,12 @@ def validate_ip(ipaddr):
             return False
     return True
 
+
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'ip_bind=', 'protected_mode='])
+                                       ['help', 'pass=',
+                                        'ip_bind=', 'protected_mode='])
     except getopt.GetoptError as e:
         usage(e)
 
@@ -63,46 +67,50 @@ def main():
         while True:
             ip_bind = d.get_input(
                 "IPv4 Address to access Redis",
-                ("Enter IPv4 Address that will be allowed "
-                 "to access the Redis instance."),
-                 DEFAULT_BIND)
+                ("Enter IPv4 Address that will be allowed"
+                 " to access the Redis instance."),
+                DEFAULT_BIND)
             if validate_ip(ip_bind):
                 break
-            d.msgbox("Invalid IPv4 Address",
+            d.msgbox(
+                "Invalid IPv4 Address",
                 "\"{}\" is not a valid IPv4 address!".format(ip_bind))
-            
+
     if not protected_mode:
         d = Dialog('TurnKey Linux - First boot configuration')
-        protected_mode = d.yesno('Keep protected-mode enabled?',
-                                 ("In this mode Redis only replies to queries from the loopback "
-                                  "interfaces. Reply to other clients connecting from other addresses "
-                                  "will receive an error, noting why & how to configure Redis. "
-                                  "(disabled by-default)"),
-                                  'Yes', 'No')
+        protected_mode = d.yesno(
+                'Keep protected-mode enabled?',
+                ("In this mode Redis only replies to queries from the loopback"
+                 " interfaces. Reply to other clients connecting from other"
+                 " addresses will receive an error, noting why & how to"
+                 " configure Redis. (disabled by-default)"),
+                'Yes', 'No')
 
-    protected_mode_string = { True: "yes", False: "no" }
+    protected_mode_string = {True: "yes", False: "no"}
     conf = "/etc/redis/redis.conf"
-    redis_commander_conf = "/etc/init.d/redis-commander" 
+    redis_commander_conf = "/etc/init.d/redis-commander"
     subprocess.run(["sed", "-i", "s|^bind .*|bind %s|" % ip_bind, conf])
     subprocess.run([
         "sed", "-i",
-        "s|^protected-mode .*|protected-mode %s|" % 
-            protected_mode_string[protected_mode],
+        "s|^protected-mode .*|protected-mode %s|" %
+        protected_mode_string[protected_mode],
         conf])
     subprocess.run([
-        "sed", "-i", 
+        "sed", "-i",
         "s|--http-auth-password=.*|--http-auth-password=%s|" %
         password, redis_commander_conf])
 
     # restart redis and redis commander if running so change takes effect
     try:
-        subprocess.run(["systemctl", "is-active", "--quiet", "redis-server.service"])
+        subprocess.run(["systemctl", "is-active",
+                        "--quiet", "redis-server.service"])
         subprocess.run(["service", "redis-server", "restart"])
     except ExecError as e:
         pass
 
     try:
-        subprocess.run(["systemctl", "is-active", "--quiet", "redis-commander.service"])
+        subprocess.run(["systemctl", "is-active",
+                        "--quiet", "redis-commander.service"])
         subprocess.run(["systemctl", "daemon-reload"])
         subprocess.run(["service", "redis-commander", "restart"])
     except ExecError as e:
