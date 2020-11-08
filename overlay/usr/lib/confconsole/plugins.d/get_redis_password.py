2655cbf971b4a0421373eed14d2a79b7498c0c20
Jeremy Davis <jeremy@turnkeylinux.org>
2020-11-09 10:20:01 +1100

    Fix the Redis Confconsole plugin - closes #1533.

--- overlay/usr/lib/confconsole/plugins.d/get_redis_password.py
+++ overlay/usr/lib/confconsole/plugins.d/get_redis_password.py
@@ -1,18 +1,23 @@
-''' Helper script for redis password '''
+'''Check Redis password'''
 
 import os
 import subprocess
-
 from subprocess import PIPE
 
 TITLE = "Check redis password"
 
+
 def run():
-    requirepass = subprocess.Popen(['turnkey-redis-pw', 'get'], stdout= subprocess.PIPE)
-    requirepass_out, requirepass_err = requirepass.communicate() 
+    requirepass = subprocess.Popen(['turnkey-redis-pw', 'get'], stdout=PIPE)
+    requirepass_out, requirepass_err = requirepass.communicate()
     if not requirepass_out:
-        console.msgbox(TITLE, "Something is wrong with redis configuration file, please check your redis.conf")        
+        console.msgbox(TITLE,
+                       "Something is wrong with redis configuration file,"
+                       " please check your redis.conf")
     else:
-        f = open ('/root/redis_password.txt', 'w+')
-        f.write(requirepass_out)
-        console.msgbox(TITLE, "Password is: {}\nsaved as /root/redis_password.txt".format(requirepass_out))
+        password = requirepass_out.decode()
+        with open('/root/redis_password.txt', 'w') as fob:
+            fob.write(password)
+        console.msgbox(TITLE,
+                       "Password is:\n\n{}\nIt has also been saved as"
+                       " /root/redis_password.txt".format(password))
