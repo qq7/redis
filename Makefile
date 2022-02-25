38b1215a6a7c40a3b453c75221e8a6c8f54714eb
qq7 <qq7dev@gmail.com>
2022-02-25 01:06:54 +0000

    v17.0

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 6379 12320 12321
 
 COMMON_CONF += tkl-webcp
-COMMON_OVERLAYS += tkl-webcp nginx
+COMMON_OVERLAYS += tkl-webcp
 
 include $(FAB_PATH)/common/mk/turnkey/nodejs.mk
+include $(FAB_PATH)/common/mk/turnkey/nginx.mk
 include $(FAB_PATH)/common/mk/turnkey.mk
---
+++ overlay/opt/tklweb-cp/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: "Redis-commander",
+    script: "/opt/tklweb-cp/node_modules/redis-commander/bin/redis-commander.js",
+    args: "--address 127.0.0.1 --port 8082 --redis-user=default --redis-password='turnkey1'",
+    env: {
+      "NODE_ENV": "production",
+      "HTTP_USER": "admin",
+      "HTTP_PASSWORD": "turnkeypw"
+    }
+  }, {
+    name: "TurnKey Linux CP",
+    script: "./tklweb-cp.js"
+  }],
+
+  deploy : {
+  }
+};
